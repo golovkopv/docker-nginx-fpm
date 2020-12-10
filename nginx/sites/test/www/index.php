d362f2e0ef07a5e2fdc936d789062e809ccb96d1
pal <golovkopv@gmail.com>
2020-12-10 13:44:50 +0200

    Initial commit.

---
+++ nginx/sites/test/www/index.php
@@ -0,0 +1,18 @@
+<?php
+  printf('
+    <HTML>
+      <BODY>
+        <br><br>
+        <div>
+          <H1 align="center">
+            <font color="GREEN">
+              This is PHP test page!!
+            </font>
+          </H1>
+        </div>
+        <br><br><br>
+      </BODY>
+    </HTML>
+  ');
+  phpinfo();
+?>
